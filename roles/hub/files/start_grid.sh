875ca807328bb983491f2663c956dd4edb2a25b0
henrrich <huanghe389@gmail.com>
2017-08-27 20:30:02 +0200

    upgrade appium gridgrid jar file in hub role

--- roles/hub/files/start_grid.sh
+++ roles/hub/files/start_grid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp *:. org.openqa.grid.selenium.GridLauncher -jar unitard-qa-grid.jar -port 4444 -role hub -hubConfig hubconfig.json >> gridhub.log 2>&1 &
+java -cp *:. org.openqa.grid.selenium.GridLauncher -jar appium-grid.jar -port 4444 -role hub -hubConfig hubconfig.json >> gridhub.log 2>&1 &
